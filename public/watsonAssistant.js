8186ca982360305030c54d211be084f921b01de2
eleonoratapparo <78021540+eleonoratapparo@users.noreply.github.com>
2021-01-26 16:04:10 +0100

    Update watsonAssistant.js

--- public/watsonAssistant.js
+++ public/watsonAssistant.js
@@ -1,11 +1,11 @@
-window.watsonAssistantChatOptions = {
-		integrationID: "19ddf878-69b7-41b3-b99b-822e8478a82d", // The ID of this integration.
-		region: "eu-gb", // The region your integration is hosted in.
-		serviceInstanceID: "02cf8e3a-8cdc-4af3-9ecf-9efe52dd6caf", // The ID of your service instance.
-		onLoad: function(instance) { instance.render(); }
-	};
+  window.watsonAssistantChatOptions = {
+      integrationID: "19ddf878-69b7-41b3-b99b-822e8478a82d", // The ID of this integration.
+      region: "eu-gb", // The region your integration is hosted in.
+      serviceInstanceID: "02cf8e3a-8cdc-4af3-9ecf-9efe52dd6caf", // The ID of your service instance.
+      onLoad: function(instance) { instance.render(); }
+    };
   setTimeout(function(){
-		const t=document.createElement('script');
+    const t=document.createElement('script');
     t.src="https://web-chat.global.assistant.watson.appdomain.cloud/loadWatsonAssistantChat.js";
     document.head.appendChild(t);
   });
